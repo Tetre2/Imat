3f74868332686dc1486e6c359de48bb2b2aa5178
Tetre2 <mattias.retteli@gmail.com>
2019-05-10 00:15:51 +0200

    Lagt till ett litet extra lager med samma funktioner som IMatDataHandler

---
+++ src/IMat.java
@@ -0,0 +1,108 @@
+import se.chalmers.cse.dat216.project.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IMat {
+
+    private IMatDataHandler dataHandler;
+
+
+    public IMat() {
+            dataHandler = IMatDataHandler.getInstance();
+        ArrayList arr = new ArrayList();
+        for (Product p: getProducts()) {
+            arr.add(p);
+        }
+
+        System.out.println(arr.size());
+
+    }
+
+
+
+    /*public Image getImage(Product p){
+        return dataHandler.getFXImage(p);
+    }*/
+
+    public void addFavorite(Product p){
+        dataHandler.addFavorite(p);
+    }
+
+    public void addProduct(Product p){
+        dataHandler.addProduct(p);
+    }
+
+    public List findProducts(String s){
+        return dataHandler.findProducts(s);
+    }
+
+    public CreditCard getCreditCard(){
+        return dataHandler.getCreditCard();
+    }
+
+    public Customer getCustomer(){
+        return dataHandler.getCustomer();
+    }
+
+    public List<Order> getOrders(){
+        return dataHandler.getOrders();
+    }
+
+    public List<Product> getProducts(ProductCategory pc){
+        return dataHandler.getProducts(pc);
+    }
+
+    public List<Product> getProducts(){
+        return dataHandler.getProducts();
+    }
+
+    public Product getProduct(int idNbr){
+        return dataHandler.getProduct(idNbr);
+    }
+
+    public ShoppingCart getShoppingCart(){
+        return dataHandler.getShoppingCart();
+    }
+
+    public User getUser(){
+        return dataHandler.getUser();
+    }
+
+
+    public boolean hasImage(Product p){
+        return dataHandler.hasImage(p);
+    }
+
+    public boolean isCustomerComplete(){
+        return dataHandler.isCustomerComplete();
+    }
+
+    public boolean isFavorite(Product p){
+        return dataHandler.isFavorite(p);
+    }
+
+    public void placeOrder(){
+        dataHandler.placeOrder();
+    }
+
+    public void placeOrder(boolean clearCart){
+        dataHandler.placeOrder(clearCart);
+    }
+
+    public boolean isFirstRun(){
+        return dataHandler.isFirstRun();
+    }
+
+    public void shutDown(){
+        dataHandler.shutDown();
+    }
+
+    public List<Product> getFavorites(){
+        return dataHandler.favorites();
+    }
+
+    public IMatDataHandler getDataHandler() {
+        return dataHandler;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import se.chalmers.cse.dat216.project.IMatDataHandler;
-import se.chalmers.cse.dat216.project.Product;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        IMatDataHandler.getInstance().addFavorite(new Product());
+        IMat iMat = new IMat();
+
     }
 }
