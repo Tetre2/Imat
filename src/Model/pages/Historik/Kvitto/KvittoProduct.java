df8f28ec76e45fef8ab79400afdd87aa22af3ac2
Tetre2 <mattias.retteli@gmail.com>
2019-05-15 19:24:35 +0200

    Kvitton säger vad de inneholler

--- src/Model/pages/Historik/Kvitto/KvittoProduct.java
+++ src/Model/pages/Historik/Kvitto/KvittoProduct.java
@@ -10,18 +10,32 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import se.chalmers.cse.dat216.project.*;
 
 import java.io.IOException;
+import java.util.List;
 
 public class KvittoProduct extends AnchorPane {
 
+    @FXML
+    private Label category;
+    @FXML
+    private VBox vBox;
 
-    public KvittoProduct(Order order) {
+    public KvittoProduct(String categoryName, List<Product> products) {
         FXMLLoader fxmlLoader = initFXML();
         tryToLoadFXML(fxmlLoader);
 
         addEventListeners();
+
+        System.out.println(categoryName);
+        category.setText(categoryName);
+
+        for (Product p : products) {
+            vBox.getChildren().add(new Label((p.getName())));
+        }
+
 
     }
 
@@ -30,7 +44,7 @@ public class KvittoProduct extends AnchorPane {
     }
 
     private FXMLLoader initFXML() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Kvitto.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("KvittoProduct.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
         return fxmlLoader;
