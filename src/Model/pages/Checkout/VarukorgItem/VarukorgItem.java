61b32929d3e625a69fb468b548944fe0828f1503
Joel Rudsberg <rudsberg@live.se>
2019-05-15 11:18:15 +0200

    added pliancy to remove button shoppingcart

--- src/Model/pages/Checkout/VarukorgItem/VarukorgItem.java
+++ src/Model/pages/Checkout/VarukorgItem/VarukorgItem.java
@@ -1,12 +1,15 @@
 package Model.pages.Checkout.VarukorgItem;
 
 import Model.IMat;
+import Model.Main;
 import Model.components.Picker.Picker;
 import Model.pages.Checkout.ShoppingCheckout.ShoppingCheckout;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -23,6 +26,7 @@ public class VarukorgItem extends AnchorPane implements ShoppingCartListener {
     @FXML private Label totalPriceLabel;
     @FXML private ImageView productImageView;
     @FXML private Button deleteButton;
+    @FXML private ImageView deleteImageView;
     @FXML private Pane pickerPane;
 
     private ShoppingCheckout shoppingCheckout;
@@ -44,12 +48,35 @@ public class VarukorgItem extends AnchorPane implements ShoppingCartListener {
 
         IMat.getInstance().getShoppingCart().addShoppingCartListener(this);
         deleteButton.setOnAction(e -> onCloseButtonPressed());
+        deleteButton.setOnMouseEntered(e -> closeButtonMouseEntered());
+        deleteButton.setOnMousePressed(e -> closeButtonMousePressed());
+        deleteButton.setOnMouseExited(e -> closeButtonMouseExited());
     }
 
     private void onCloseButtonPressed() {
         shoppingItem.setAmount(0);
         IMat.getInstance().getShoppingCart().fireShoppingCartChanged(shoppingItem, true);
         shoppingCheckout.removeShoppingItemFromUI(this);
+    }
+
+    @FXML
+    public void closeButtonMouseEntered(){
+        Main.getCurrentScene().setCursor(Cursor.HAND);
+        deleteImageView.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "Model/resources/icon_close_hover.png")));
+    }
+
+    @FXML
+    public void closeButtonMousePressed(){
+        deleteImageView.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "Model/resources/icon_close_pressed.png")));
+    }
+
+    @FXML
+    public void closeButtonMouseExited(){
+        Main.getCurrentScene().setCursor(Cursor.DEFAULT);
+        deleteImageView.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "Model/resources/icon_close.png")));
     }
 
     private FXMLLoader initFXML() {
