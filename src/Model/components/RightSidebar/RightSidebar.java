f51b7bf67249d76fa59ecd08c85b5a795fe37832
Joel Rudsberg <rudsberg@live.se>
2019-05-21 15:41:23 +0200

    pliancy delete button right panel

--- src/Model/components/RightSidebar/RightSidebar.java
+++ src/Model/components/RightSidebar/RightSidebar.java
@@ -1,12 +1,15 @@
 package Model.components.RightSidebar;
 
 import Model.IMat;
+import Model.Main;
 import Model.components.RightSidebar.RightSidebarItem.RightSidebarItem;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
@@ -59,7 +62,6 @@ public class RightSidebar extends AnchorPane implements ShoppingCartListener {
             throw new RuntimeException(exception);
         }
     }
-
 
     private void addEventListeners() {
         pay.setOnAction(e -> onPayPressed());
